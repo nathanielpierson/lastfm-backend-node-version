1233b1a977b47aa49a074aeec54340f7285dbe67
nathanielpierson <nathanielepierson@gmail.com>
2025-06-17 23:46:19 -0400

    add basic GET request

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello Wooooooorld!');
+});
+
+app.post('/', (req, res) => {
+  res.send('POST request to the homepage');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
