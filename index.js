d849e50f50ad22aaefdf004591d6da871fa3aa81
nathanielpierson <nathanielepierson@gmail.com>
2025-06-21 00:45:54 -0400

    working basic axios request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import axios from "axios";
 
 // Import route modules
 import artistRoutes from "./routes/artistRoutes.js";
@@ -21,6 +22,30 @@ app.use("/api/artist", artistRoutes);
 // Root route (optional)
 app.get("/", (req, res) => {
   res.send("🎵 Last.fm Proxy API is running!");
+});
+
+app.get("/top-tracks", async (req, res) => { 
+  const artist = req.query.artist;
+
+  if (!artist) {
+    return res.status(400).json({ error: 'Artist is required' });
+  }
+
+  try {
+    const response = await axios.get('http://ws.audioscrobbler.com/2.0/', {
+      params: {
+        method: 'artist.gettoptracks',
+        artist,
+        api_key: process.env.LASTFM_API_KEY,
+        format: 'json'
+      }
+    });
+
+    res.json(response.data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Failed to fetch data from Last.fm' });
+  }
 });
 
 // Start server
